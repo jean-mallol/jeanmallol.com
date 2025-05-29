2b579789caecc26447cab535e4036a179fc3ef0a
Jean <jean.mallol@fdti.eu>
2025-05-29 16:48:28 +0200

    fix et og image

--- src/lib/components/SEO.svelte
+++ src/lib/components/SEO.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let title: string;
 	export let description: string;
-	export let image: string = '/og-image.jpg';
+	export let image: string = '/og-image.svg';
 	export let url: string = 'https://jeanmallol.com';
 </script>
 
